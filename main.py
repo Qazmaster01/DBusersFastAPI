1b6a66181409cce635a91206064e2a56f79b2fdc
Kaden Aset <zakaset20000@gmail.com>
2024-02-27 09:46:10 +0600

    feat: Обработка ошибок

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Body, HTTPException
 from pydantic import BaseModel, Field, validator
 from typing import List
 from typing import Annotated
-app = FastAPI(debug=True)
+app = FastAPI()
 
 fake_user = []
-
-@app.get('/infor')
-def user(limit: int=0, offset: int=0):
-    return fake_user[offset:][:limit]
-
-@app.post('/users/{id}')
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_user))[0]
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
-
 
 #Пользватель
 class Userskz(BaseModel):
@@ -36,19 +25,18 @@ class Userskz(BaseModel):
             if value.find('.') > value.find('@'):
                 a = value.index('@')
                 if not (value[a + 1:a + 5] == 'mail' or value[a + 1:a + 6] == 'gmail'):
-                    raise TypeError('Должно быть mail или gmail')
+                    raise HTTPException(status_code=500, detail='Должно быть mail или gmail')
                 if value[a + 1:a + 5] == 'mail':
                     if not value[-3:] == '.ru':
-                        raise TypeError('Проверьте правильность электронной почты')
+                        raise HTTPException(status_code=500, detail='Проверьте правильность электронной почты')
                 if value[a + 1:a + 6] == 'gmail':
                     if not value[-4:] == '.com':
-                        raise TypeError('Проверьте правильность электронной почты')
+                        raise HTTPException(status_code=500, detail='Проверьте правильность электронной почты')
                 return value
             else:
-                raise TypeError('@ должен быть перед .')
+                raise HTTPException(status_code=500, detail='@ должен быть перед .')
         else:
-            raise TypeError('@ или . должен быть ровно 1 раз')
-
+            raise HTTPException(status_code=500, detail='@ или . должен быть ровно 1 раз')
     #валидация name
     @validator("name")
     @classmethod
@@ -56,7 +44,8 @@ class Userskz(BaseModel):
         if len(value) < 2 or len(value) > 10:
             raise TypeError('Имя меньше 2 букв')
         if not value.istitle():
-            raise TypeError('Имя должен начинаться с заглавной буквой')
+            raise HTTPException(status_code=500, detail='Имя должен начинаться с заглавной буквой')
+            # raise TypeError('Имя должен начинаться с заглавной буквой')
         return value
 
     #валидация phone_number
@@ -67,18 +56,18 @@ class Userskz(BaseModel):
             if str(value[:2]) == '87':
                 return value
             else:
-                raise TypeError('Неправильный номер')
+                raise HTTPException(status_code=500, detail='Неправильный номер')
         else:
-            raise TypeError('Номер меньше 11')
+            raise HTTPException(status_code=500, detail='Номер меньше 11')
 
     #валидация surname
     @validator("surname")
     @classmethod
     def validate_surname(cls, value):
         if len(value) < 2 or len(value) > 10:
-            raise TypeError('Фамилия меньше 2 букв')
+            raise HTTPException(status_code=500, detail='Фамилия меньше 2 букв')
         if not value.istitle():
-            raise TypeError('Фамилия должно начинаться с заглавной буквой')
+            raise HTTPException(status_code=500, detail='Фамилия должно начинаться с заглавной буквой')
         return value
 
     #валидация fatherland
@@ -86,9 +75,9 @@ class Userskz(BaseModel):
     @classmethod
     def validate_fatherland(cls, value):
         if len(value) < 2 or len(value) > 10:
-            raise TypeError('Отечество меньше 2 букв')
+            raise HTTPException(status_code=500, detail='Отечество меньше 2 букв')
         if not value.istitle():
-            raise TypeError('Отечество должно начинаться с заглавной буквой')
+            raise HTTPException(status_code=500, detail='Отечество должно начинаться с заглавной буквой')
         return value
 
     #валидация gender
@@ -98,7 +87,7 @@ class Userskz(BaseModel):
         if value == "Мужчина" or value == "Женщина":
             return value
         else:
-            raise TypeError('Неверный гендер. Укажите гендер так (Мужчина - если вы мужчина) или (Женщина - если вы женщина)')
+            raise HTTPException(status_code=500, detail='Неверный гендер. Укажите гендер так (Мужчина - если вы мужчина) или (Женщина - если вы женщина)')
 
     #валидаация password
     @validator("password")
@@ -120,42 +109,47 @@ class Userskz(BaseModel):
                 if i in s:
                     total_s += 1
             if total_N < 3:
-                raise ValueError('В пароле верхний регистр меньше 3')
+                raise HTTPException(status_code=500, detail='В пароле верхний регистр меньше 3')
             if total_e < 6:
-                raise ValueError('В пароле нижний регистр меньше 6')
+                raise HTTPException(status_code=500, detail='В пароле нижний регистр меньше 6')
             if total_n < 3:
-                raise ValueError('В пароле цифры меньше 3')
+                raise HTTPException(status_code=500, detail='В пароле цифры меньше 3')
             if total_s < 1:
-                raise ValueError('В пароле спецсимволы меньше 1')
+                raise HTTPException(status_code=500, detail='В пароле спецсимволы меньше 1')
             if total_N >= 2 or total_e > 6 or total_n > 3 or total_s > 1:
                 return value
         else:
-            raise ValueError('В пароле длина пароля меньше 12')
+            raise HTTPException(status_code=500, detail='В пароле длина пароля меньше 12')
 
 
 # @app.post('/users')
 # def users_kz(users: List[Userskz]):
 #     fake_user.extend(users)
 #     return {'status': 200, 'data': fake_user}
-user = []
 
+
+# @app.post("/login/")
+# async def login(
+#     name: str = Form(...),
+#     surname: str = Form(...),
+#     fatherland: str = Form(...),
+#     gender: str = Form(...),
+#     phone_number: str = Form(...),
+#     email: str = Form(...),
+#     password: str = Form(...),
+# ):
+#     user.append({
+#         "name": name,
+#         "surname": surname,
+#         "fatherland": fatherland,
+#         "gender": gender,
+#         "phone_number": phone_number,
+#         "email": email,
+#         "password": password
+#     })
+#     return {"name": name, "surname": surname, "fatherland": fatherland, "gender": gender, "phone_number": phone_number, "email": email, "password": password}
+user = []
 @app.post("/login/")
-async def login(
-    name: str = Form(...),
-    surname: str = Form(...),
-    fatherland: str = Form(...),
-    gender: str = Form(...),
-    phone_number: str = Form(...),
-    email: str = Form(...),
-    password: str = Form(...),
-):
-    user.append({
-        "name": name,
-        "surname": surname,
-        "fatherland": fatherland,
-        "gender": gender,
-        "phone_number": phone_number,
-        "email": email,
-        "password": password
-    })
-    return {"name": name, "surname": surname, "fatherland": fatherland, "gender": gender, "phone_number": phone_number, "email": email, "password": password}
+async def login(users: List[Userskz] = Body(...)):
+    fake_user.extend(users)
+    return {'data': fake_user}
