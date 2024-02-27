f7f90e0a76851c4e9cab5c30a3b709410f69c3dc
Kaden Aset <zakaset20000@gmail.com>
2024-02-27 09:59:18 +0600

    feat: Добавлен форма

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI, Form, Body, HTTPException
 from pydantic import BaseModel, Field, validator
 from typing import List
 from typing import Annotated
+
 app = FastAPI()
 
-fake_user = []
+new_user = []
 
-#Пользватель
 class Userskz(BaseModel):
     name: str
     surname: str
@@ -16,10 +16,7 @@ class Userskz(BaseModel):
     email: str
     password: str
 
-
-    #валидация email
     @validator("email")
-    @classmethod
     def validate_email(cls, value):
         if value.count('@') == 1 and value.count('.') == 1:
             if value.find('.') > value.find('@'):
@@ -37,20 +34,16 @@ class Userskz(BaseModel):
                 raise HTTPException(status_code=500, detail='@ должен быть перед .')
         else:
             raise HTTPException(status_code=500, detail='@ или . должен быть ровно 1 раз')
-    #валидация name
+
     @validator("name")
-    @classmethod
     def validate_name(cls, value):
         if len(value) < 2 or len(value) > 10:
-            raise TypeError('Имя меньше 2 букв')
+            raise HTTPException(status_code=500, detail='Имя должно быть от 2 до 10 символов')
         if not value.istitle():
-            raise HTTPException(status_code=500, detail='Имя должен начинаться с заглавной буквой')
-            # raise TypeError('Имя должен начинаться с заглавной буквой')
+            raise HTTPException(status_code=500, detail='Имя должно начинаться с заглавной буквы')
         return value
 
-    #валидация phone_number
     @validator("phone_number")
-    @classmethod
     def validate_phone_number(cls, value):
         if len(str(value)) <= 11:
             if str(value[:2]) == '87':
@@ -58,40 +51,32 @@ class Userskz(BaseModel):
             else:
                 raise HTTPException(status_code=500, detail='Неправильный номер')
         else:
-            raise HTTPException(status_code=500, detail='Номер меньше 11')
+            raise HTTPException(status_code=500, detail='Номер должен быть не более 11 символов')
 
-    #валидация surname
     @validator("surname")
-    @classmethod
     def validate_surname(cls, value):
         if len(value) < 2 or len(value) > 10:
-            raise HTTPException(status_code=500, detail='Фамилия меньше 2 букв')
+            raise HTTPException(status_code=500, detail='Фамилия должна быть от 2 до 10 символов')
         if not value.istitle():
-            raise HTTPException(status_code=500, detail='Фамилия должно начинаться с заглавной буквой')
+            raise HTTPException(status_code=500, detail='Фамилия должна начинаться с заглавной буквы')
         return value
 
-    #валидация fatherland
     @validator("fatherland")
-    @classmethod
     def validate_fatherland(cls, value):
         if len(value) < 2 or len(value) > 10:
-            raise HTTPException(status_code=500, detail='Отечество меньше 2 букв')
+            raise HTTPException(status_code=500, detail='Отчество должно быть от 2 до 10 символов')
         if not value.istitle():
-            raise HTTPException(status_code=500, detail='Отечество должно начинаться с заглавной буквой')
+            raise HTTPException(status_code=500, detail='Отчество должно начинаться с заглавной буквы')
         return value
 
-    #валидация gender
     @validator("gender")
-    @classmethod
     def validate_gender(cls, value):
         if value == "Мужчина" or value == "Женщина":
             return value
         else:
-            raise HTTPException(status_code=500, detail='Неверный гендер. Укажите гендер так (Мужчина - если вы мужчина) или (Женщина - если вы женщина)')
+            raise HTTPException(status_code=500, detail='Неверный гендер. Укажите "Мужчина" или "Женщина"')
 
-    #валидаация password
     @validator("password")
-    @classmethod
     def validate_password(cls, value):
         s = '!@#$%^&*'
         total_e = 0
@@ -109,17 +94,56 @@ class Userskz(BaseModel):
                 if i in s:
                     total_s += 1
             if total_N < 3:
-                raise HTTPException(status_code=500, detail='В пароле верхний регистр меньше 3')
+                raise HTTPException(status_code=500, detail='В пароле должно быть не менее 3 символов в верхнем регистре')
             if total_e < 6:
-                raise HTTPException(status_code=500, detail='В пароле нижний регистр меньше 6')
+                raise HTTPException(status_code=500, detail='В пароле должно быть не менее 6 символов в нижнем регистре')
             if total_n < 3:
-                raise HTTPException(status_code=500, detail='В пароле цифры меньше 3')
+                raise HTTPException(status_code=500, detail='В пароле должно быть не менее 3 цифр')
             if total_s < 1:
-                raise HTTPException(status_code=500, detail='В пароле спецсимволы меньше 1')
-            if total_N >= 2 or total_e > 6 or total_n > 3 or total_s > 1:
-                return value
+                raise HTTPException(status_code=500, detail='В пароле должен быть хотя бы 1 спецсимвол')
+            return value
         else:
-            raise HTTPException(status_code=500, detail='В пароле длина пароля меньше 12')
+            raise HTTPException(status_code=500, detail='Пароль должен содержать не менее 12 символов')
+
+@app.post("/login/")
+async def login(
+    name: str = Form(...),
+    surname: str = Form(...),
+    fatherland: str = Form(...),
+    gender: str = Form(...),
+    phone_number: str = Form(...),
+    email: str = Form(...),
+    password: str = Form(...)
+):
+    try:
+        user = Userskz(
+            name=name,
+            surname=surname,
+            fatherland=fatherland,
+            gender=gender,
+            phone_number=phone_number,
+            email=email,
+            password=password
+        )
+    except HTTPException as a:
+        return {"error": str(a.detail)}
+    else:
+        new_user.append(user)
+        return {'data': new_user}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # @app.post('/users')
@@ -148,8 +172,3 @@ class Userskz(BaseModel):
 #         "password": password
 #     })
 #     return {"name": name, "surname": surname, "fatherland": fatherland, "gender": gender, "phone_number": phone_number, "email": email, "password": password}
-user = []
-@app.post("/login/")
-async def login(users: List[Userskz] = Body(...)):
-    fake_user.extend(users)
-    return {'data': fake_user}
