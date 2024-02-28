7ff6173052676600a282a5de10b7085dcee163f8
Kaden Aset <zakaset20000@gmail.com>
2024-02-28 22:30:04 +0600

    feat: Добавил поддержку

--- configur.py
+++ configur.py
@@ -1,10 +1,9 @@
-from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+
+from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
