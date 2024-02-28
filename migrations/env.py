5f2a21ee4950954aca6207aa9256c8b91c823449
Kaden Aset <zakaset20000@gmail.com>
2024-02-28 22:28:27 +0600

    feat: Fix

--- migrations/env.py
+++ migrations/env.py
@@ -1,14 +1,26 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from models.models import metadata
+
+# Эта строка импорта может отличаться в зависимости от структуры вашего проекта
+# Убедитесь, что она указывает на правильное местоположение вашей модели
+# Например, если ваша модель находится в файле models.py в корне вашего проекта, строка должна быть такой:
+# from models import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+section = config.config_ini_section
+config.set_section_option(section, "DB_HOST", DB_HOST)
+config.set_section_option(section, "DB_PORT", DB_PORT)
+config.set_section_option(section, "DB_USER", DB_USER)
+config.set_section_option(section, "DB_NAME", DB_NAME)
+config.set_section_option(section, "DB_PASS", DB_PASS)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -18,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -43,7 +55,7 @@ def run_migrations_offline() -> None:
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        dialect_opts={"paramstyle": "named", "client_encoding": "utf8"},  # добавляем опцию client_encoding
     )
 
     with context.begin_transaction():
@@ -65,7 +77,7 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata, dialect_opts={"client_encoding": "utf8"}  # добавляем опцию client_encoding
         )
 
         with context.begin_transaction():
---
+++ models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(BaseModel):
+    __tablename__ = 'User'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    fatherland = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    phone_number = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
