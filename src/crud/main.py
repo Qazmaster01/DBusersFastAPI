8dd37aede3f60febd1099d0148b4a58210d28dac
Kaden Aset <zakaset20000@gmail.com>
2024-02-29 17:01:35 +0600

    fix: Modified

--- src/crud/main.py
+++ src/crud/main.py
@@ -4,10 +4,11 @@ from models import User
 from pydantic import BaseModel, EmailStr, validator
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import bcrypt
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 
@@ -18,6 +19,7 @@ DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 
 
@@ -130,7 +132,7 @@ class UserResponse(BaseModel):
     fatherland: str
     gender: str
     phone_number: str
-    email: EmailStr
+    email: str
 
 # Routes
 @app.post("/users/", response_model=UserResponse)
@@ -156,10 +158,39 @@ async def read_user(user_id: int):
     db = SessionLocal()
     user = db.query(User).filter(User.id == user_id).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Пользователь не найдено")
+    return user
+
+@app.put("/users/{user_id}", response_model=UserResponse)
+async def update_user(user_id: int, name: str, surname: str, fatherland: str, gender: str, phone_number: str, email: str, password: str):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найдено")
+    user.name = name
+    user.surname = surname
+    user.fatherland = fatherland
+    user.gender = gender
+    user.phone_number = phone_number
+    user.email = email
+    user.password = Hasher.get_password_hash(password)
+    db.commit()
+    db.refresh(user)
     return user
 
 
+
+
+
+@app.delete("/users/remove", response_model=UserResponse)
+async def remove_user(user_id: int):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="Пользователь не найдено")
+    db.delete(user)
+    db.commit()
+    return user
 
 # @app.post('/users')
 # def users_kz(users: List[Userskz]):
