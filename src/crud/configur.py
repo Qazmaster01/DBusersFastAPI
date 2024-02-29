5cf806ecc39ed727fdfa3bed5c43e9c079cb8d3f
Kaden Aset <zakaset20000@gmail.com>
2024-02-29 17:03:33 +0600

    feat: Изменен путь

--- src/crud/configur.py
+++ src/crud/configur.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from src.crud.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
